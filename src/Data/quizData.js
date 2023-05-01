2c9c0445d9c1096a232789a8d069ed0d0547ef75
unknown <rajwalmiki772000@gmail.com>
2023-05-01 09:51:01 +0530

    01-05-2023

---
+++ src/Data/quizData.js
@@ -0,0 +1,38 @@
+export const QuizData = [
+    {
+        question: "Javascript is a __ language.",
+        options:[ "Programming", "Application", "Scripting", "None of the above"],
+        answer: 3
+    },
+    {
+        question: "JavaScript is a __ Side Scripting Language.",
+        options:["Server", "Browser", "ISP", "None of the above"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "JavaScript can be written",
+        options: ["directly on the Server Script", "directly into HTML pages", "All of the above", "none of the above"],
+        answer:2
+    },
+    {
+        question: "JavaScript code is written inside file having extension",
+        options: [".jvs", ".js", ".jsc", ".javascript"],
+        answer:2
+    },
+
+    {
+        question: "JavaScript is also called as",
+        options: ["Server Side Scripting Language", "Client Side Scripting Language", "All of the above", "none of the above"],
+        answer:2
+    },
+];
